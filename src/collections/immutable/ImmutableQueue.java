a9aafee20bee6153426fcb8f9733ddeb0a03f6c4
Tangi <tangui.favennec@gmail.com>
2020-01-21 15:16:26 +0900

    Initial commit : Project Structure base

---
+++ src/collections/immutable/ImmutableQueue.java
@@ -0,0 +1,26 @@
+package collections.immutable;
+
+import collections.Queue;
+
+public class ImmutableQueue<T> implements Queue<T> {
+
+    @Override
+    public Queue enQueue(T item) {
+        return null;
+    }
+
+    @Override
+    public Queue deQueue() {
+        return null;
+    }
+
+    @Override
+    public T head() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
