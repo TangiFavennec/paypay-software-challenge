49905d125b0ee2d285aaeabdb99fb665319275f6
Tangi <tangui.favennec@gmail.com>
2020-01-21 15:42:21 +0900

    Add Immutable Stack

---
+++ src/collections/immutable/ImmutableStack.java
@@ -0,0 +1,22 @@
+package collections.immutable;
+
+import javafx.util.Pair;
+
+public class ImmutableStack<T> {
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public Pair<T, ImmutableStack<T>> peek() {
+        return null;
+    }
+
+    public Pair<T, ImmutableStack<T>> pop() {
+        return null;
+    }
+
+    public ImmutableStack<T> push(final T input) {
+        return null;
+    }
+}
